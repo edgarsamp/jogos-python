e692a3784340a145755f044e5eec14a1648d17b7
Edgar Sampaio de Barros <edgar-sampaio@hotmail.com>
2021-11-07 18:31:33 -0300

    Criando a estrutura basica dos arquivos dos jogos

--- jogos/jogoAdivinhacao.py
+++ jogos/jogoAdivinhacao.py
@@ -1,1 +1,5 @@
+def jogo_de_adivinhacao():
+    print("Jogo da velha")
 
+if __name__ == "__main__":
+    jogo_de_adivinhacao()
---
+++ jogos/jogoDaVelha.py
@@ -0,0 +1,5 @@
+def jogo_da_velha():
+    print("Jogo da velha")
+
+if __name__ == "__main__":
+    jogo_da_velha()
