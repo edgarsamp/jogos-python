e2bcea7b78aecedf01d27712574901b545c2dcca
Edgar Sampaio de Barros <edgar-sampaio@hotmail.com>
2021-11-09 09:58:42 -0300

    Criacao do menu para o jogo de adivinhacao

--- jogos/jogoAdivinhacao.py
+++ jogos/jogoAdivinhacao.py
@@ -1,5 +1,13 @@
 def jogo_de_adivinhacao():
-    print("Jogo da velha")
+    print("Jogo de adivinhacao")
+    while(True):
+        opcao = int(input('1 - Adivinhar um numero\n2 - Ter um numero adivinhado\n'))
+        if(opcao == 1 or opcao == 2):
+            break
+    if opcao == 1:
+        adivinhar()
+    else:
+        adivinhado()
 
 if __name__ == "__main__":
     jogo_de_adivinhacao()
