b08eb703fcb2a2b324a2c18878166c40b5fdcb77
Edgar Sampaio de Barros <edgar-sampaio@hotmail.com>
2021-11-09 11:25:48 -0300

    Implementação da função adivinhado

--- jogos/jogoAdivinhacao.py
+++ jogos/jogoAdivinhacao.py
@@ -8,6 +8,7 @@ def jogo_de_adivinhacao():
             break
     if opcao == 1:
         adivinhar()
+        
     else:
         adivinhado()
 
@@ -15,8 +16,8 @@ def adivinhar():
     chances = 5
     tentativas = []
 
-    numero = random.randrange(20)
-    print("Foi sorteado aleatoriamente um numero entre 0 e 20")
+    numero = random.randrange(1, 20)
+    print("Foi sorteado aleatoriamente um numero entre 1 e 20")
     print(f"Voce tem {chances} chances")
     while(chances > 0):
         n = int(input("De seu chute: "))
@@ -36,8 +37,31 @@ def adivinhar():
         print("Parabens voce conseguiu adivinhar o numero e ainda sobraram", chances, "tentativas!")
 
 def adivinhado():
-    print("adivinhado")
+    print("Escolha um numero de 1 a 20, o programa tentara adivinhar e voce deve respoder com:")
+    print("maior   - caso o chute do computador for menor que seu numero")
+    print("maior   - caso o chute do computador for menor que seu numero")
+    print("acertou - caso o chute do computador estiver certo")
 
+    #Busca binaria
+    l = 1
+    r = 20
+    tentativas = 0
+
+    while(r > l+1):
+        tentativas += 1
+        chute = (l+r)//2
+        check = input(f"Chute: {chute} -> ")
+        if(check.lower() == "acertou"):
+            break
+        if(check.lower() == "maior"):
+            l = chute
+        elif(check.lower() == "menor"):
+            r = chute
+        else:
+            print("Entrada invalida")
+            tentativas -= 1
+        
+    print(f"O computador precisou de {tentativas} para acertar seu numero")
 
 if __name__ == "__main__":
     jogo_de_adivinhacao()
